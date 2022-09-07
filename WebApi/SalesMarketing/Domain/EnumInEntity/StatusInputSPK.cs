f66de01ef1ad8e0752e1b3bceb9df084da75460f
Sutanto Gasali <43228630+BionStt@users.noreply.github.com>
2022-09-07 17:25:17 +0700

    add enuminEntity

---
+++ WebApi/SalesMarketing/Domain/EnumInEntity/StatusInputSPK.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.SalesMarketing.Domain.EnumInEntity
+{
+    public class StatusInputSPK
+    {
+        private StatusInputSPK(string statusInputSPKKeterangan)
+        {
+            StatusInputSPKId = Guid.NewGuid();
+            StatusInputSPKKeterangan = statusInputSPKKeterangan;
+        }
+        public static StatusInputSPK AddStatusInputSPK(string statusInputSPKKeterangan)
+        {
+            return new StatusInputSPK(statusInputSPKKeterangan);
+        }
+        public Guid StatusInputSPKId { get; set; }
+        public int NoUrutId { get; set; }
+        public String StatusInputSPKKeterangan { get; set; }
+    }
+
+    //public enum StatusInputSPK
+    //{
+    //    Disurvei = 0,
+    //    DiAcc = 1,
+    //    Ditolaks = 2,
+    //    DiKirimUnit = 3
+    //}
+
+}
