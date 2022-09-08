f0f456203b5739e62d6c93e4857843d31f223621
Sutanto Gasali <43228630+BionStt@users.noreply.github.com>
2022-09-08 10:50:36 +0700

    revisi

--- WebApi/SalesMarketing/Infrastructure/Configuration/JenisKelaminConfiguration.cs
+++ WebApi/SalesMarketing/Infrastructure/Configuration/JenisKelaminConfiguration.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApi.SalesMarketing.Domain.EnumInEntity;
 
 namespace WebApi.SalesMarketing.Infrastructure.Configuration
 {
